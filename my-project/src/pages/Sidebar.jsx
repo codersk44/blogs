b01d0a81344a5490ed130ceb066d3d8a9172490e
codersk44 <shashankmakwana44@gmail.com>
2023-11-29 18:45:33 +0530

    first commit

---
+++ my-project/src/pages/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+
+
+        <h3 className='text-3xl font-semibold  px-4 ' > Letest Blogs </h3>
+    </div>
+  )
+}
+
+export default Sidebar
